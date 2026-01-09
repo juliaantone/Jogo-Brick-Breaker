271edd5d45104aa8cd8ecf2a17b537546ecc9e4b
Julia Antonelly <siqueira.a@academico.ifrn.edu.br>
2026-01-09 19:01:04 -0300

    enviar

--- brick_breaker.py
+++ brick_breaker.py
@@ -56,7 +56,6 @@ def criar_blocos(nivel):
     largura_da_tela = tamanho_da_tela[0]
     largura_total_da_tela = (8 * (85)) + (7 * espaco_x)
     x_inicial = (largura_da_tela - largura_total_da_tela) // 2
-
     y_inicial = 50
 
     if nivel == 3:
