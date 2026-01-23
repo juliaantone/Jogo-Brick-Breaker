4e43c1bc7ad4cea7eaefee4f407373bfa2133c9a
Julia Antonelly <siqueira.a@academico.ifrn.edu.br>
2026-01-22 21:14:11 -0300

    enviar

--- brick_breaker.py
+++ brick_breaker.py
@@ -24,7 +24,7 @@ cores = {
 
 fonte_do_titulo = pygame.font.SysFont("impact", 68)
 fonte_do_botao = pygame.font.SysFont("arial bold", 36)
-fonte_vidas_nivel = pygame.font.SysFont("arial", 24)
+fonte_vidas_nivel = pygame.font.SysFont("times new roman", 24)
 fonte_pontuacao = pygame.font.SysFont("arial bold", 44)
 
 vidas = 5
